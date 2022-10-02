b41a4c1d93ad611637ef6d42d8a463f87eafc0ee
Anil K Rajamoni <anil.k.rajamoni99@gmail.com>
2022-10-02 08:36:21 +0530

    added solution of Q1

---
+++ Question-1/question1.py
@@ -0,0 +1,24 @@
+input_filename = "CS638_A.txt"
+output_filename = "Student_details.txt"
+ID = "CS638AS"
+#reading the input file
+with open(input_filename,mode="r") as f1:
+    inpData = f1.read().split("\n")
+    filteredData=[]
+
+    #filtering only needed lines
+    for d in range(len(inpData)):
+        if(inpData[d]!=""):
+            filteredData.append(inpData[d])
+
+    # storing in dictionary with key:value 
+    outputDict = {ID+str(i):filteredData[i].replace(",","") for i in range(1,len(filteredData))}
+    
+    #print(outputDict)
+    
+    #writing to the output file .
+
+    with open(output_filename , mode="w") as wf:
+        wf.write("ID \t\t\t\t NAME\n\n")
+        for key, val in outputDict.items():
+            wf.write("%s \t\t\t %s\n"%(key,val))
---
+++ README.md
@@ -0,0 +1,10 @@
+# PyAssignments
+
+# How To RUN the file .
+
+1. Open the command prompt or any terminal in required path 
+2. Type 
+    python <filename.py>
+    python question1.py
+
+3. It will create a new text file with name 'Student_details'
